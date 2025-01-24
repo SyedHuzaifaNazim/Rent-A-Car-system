9385e6fad9d67afd67a8f36e9ac164083997f74d
SyedHuzaifaNazim <huzaifanazim154@gmail.com>
2025-01-24 23:12:46 +0500

    adding jwt

---
+++ Backend/src/helper/tokenHandle.js
@@ -0,0 +1,22 @@
+import jwt from "jsonwebtoken";
+import "dotenv/config";
+
+const authentication = (req, res, next) => {
+  const token = req.cookies?.token;
+
+  if (!token) {
+    // return res.status(401).json({ message: "Token missing" });
+    return;
+  }
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) {
+      return res.status(403).json({ message: "Invalid or expired token" });
+    }
+    req.user = user;
+    console.log(user);
+    next();
+  });
+};
+
+export default authentication;
